7b27184944bf789924225424b0b9ca8fae25089e
Ivaylo Ivanov <ivailo1224@gmail.com>
2025-07-09 15:54:51 +0300

    items catalog extra filters

--- SteamApp.Client/src/app/components/items/items-catalog/items-catalog.component.ts
+++ SteamApp.Client/src/app/components/items/items-catalog/items-catalog.component.ts
@@ -4,13 +4,23 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import * as XLSX from 'xlsx';
 import { Item } from '../../../models/item.model';
 import { Router } from '@angular/router';
 import { ItemService } from '../../../services/item/item.service';
-import { Class, classesCollection, classesMap } from '../../../models/enums/class.enum';
-import { Slot, slotsCollection, slotsMap } from '../../../models/enums/slot.enum';
+import {
+  Class,
+  classesCollection,
+  classesMap,
+} from '../../../models/enums/class.enum';
+import {
+  Slot,
+  slotsCollection,
+  slotsMap,
+} from '../../../models/enums/slot.enum';
+import { ItemSearchComponent } from '../../item-search/item-search.component';
+import { CONSTANTS } from '../../../common/constants';
 
 @Component({
   selector: 'steam-items-catalog',
@@ -22,6 +32,8 @@ import { Slot, slotsCollection, slotsMap } from '../../../models/enums/slot.enum
     MatSortModule,
     MatPaginatorModule,
     FormsModule,
+    ReactiveFormsModule,
+    ItemSearchComponent,
   ],
   templateUrl: './items-catalog.component.html',
   styleUrl: './items-catalog.component.scss',
@@ -30,10 +42,17 @@ export class ItemsCatalogComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  private _constants = CONSTANTS;
+
+  readonly weaponUrlPartial: string =
+    this._constants.LISTING_URL_PARTIAL +
+    this._constants.WEAPON_URL_QUERY_PARAMS;
+
   dataSource = new MatTableDataSource<Item>();
   displayedColumns: string[] = [
     'id',
     'name',
+    'url',
     'is_active',
     'is_weapon',
     'class_id',
@@ -41,17 +60,19 @@ export class ItemsCatalogComponent implements OnInit, AfterViewInit {
     'actions',
   ];
 
-      classes = classesCollection;
-      getClassLabel(id: Class) {
-        console.log(classesMap[id])
-        return classesMap[id];
-      }
-    
-      slots = slotsCollection;
-    
-      getSlotLabel(id: Slot) {
-        return slotsMap[id];
-      }
+  searchControl = new FormControl<string>('', { nonNullable: true });
+  searchByItemName: string = '';
+
+  selectedClasses: number[] = [];
+  classes = classesCollection;
+
+  selectedSlots: number[] = [];
+  slots = slotsCollection;
+
+  isActive: boolean = false;
+  isWeapon: boolean = false;
+  isNotWeapon: boolean = false;
+  isInactive: boolean = false;
 
   constructor(private router: Router, private itemService: ItemService) {}
 
@@ -69,6 +90,28 @@ export class ItemsCatalogComponent implements OnInit, AfterViewInit {
       this.dataSource.paginator.pageSize = 10;
       this.dataSource.paginator.pageSizeOptions = [5, 10, 25];
     }
+  }
+
+  getClassLabel(id: Class) {
+    return classesMap[id];
+  }
+
+  getSlotLabel(id: Slot) {
+    return slotsMap[id];
+  }
+
+  onClassesChanged(newClasses: number[]) {
+    this.selectedClasses = newClasses;
+  }
+
+  onSlotsChanged(newSlots: number[]) {
+    this.selectedSlots = newSlots;
+  }
+
+  onSearchByNameChange(value: string) {
+    this.searchByItemName = value;
+
+    this.fetchItems();
   }
 
   createButtonClicked(): void {
@@ -120,12 +163,22 @@ export class ItemsCatalogComponent implements OnInit, AfterViewInit {
     });
   }
 
-  private fetchItems(): void {
-    this.itemService.getItems().subscribe({
-      next: (items) => {
-        this.dataSource.data = items;
-      },
-      error: (err) => console.error('Error fetching items:', err),
-    });
-  }
+  fetchItems = (nameFilter: string = this.searchByItemName): void => {
+    this.itemService
+      .getItems(this.selectedClasses, this.selectedSlots, nameFilter)
+      .subscribe({
+        next: (items) => {
+          const filteredData = items.filter((item) => {
+            if (this.isActive && !item.isActive) return false;
+            if (this.isInactive && item.isActive) return false;
+            if (this.isWeapon && !item.isWeapon) return false;
+            if (this.isNotWeapon && item.isWeapon) return false;
+            return true;
+          });
+
+          this.dataSource.data = filteredData;
+        },
+        error: (err) => console.error('Error fetching items:', err),
+      });
+  };
 }
