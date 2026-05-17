ff255158db96d474f2282cd445adbdd4b6f037d6
Ivaylo Ivanov <ivailo1224@gmail.com>
2026-05-17 17:43:12 +0300

    add cookie to pixel scrape

--- SteamApp.Server/SteamApp.Infrastructure/Services/SteamService.cs
+++ SteamApp.Server/SteamApp.Infrastructure/Services/SteamService.cs
@@ -111,6 +111,17 @@ public class SteamService(ISteamRepository steamRepository) : ISteamService
 
         driver.Navigate().GoToUrl(url);
 
+        // Add Cookie to redirect to old site
+        var cookie = new Cookie(
+        name: "bMarketOptOut",
+        value: "1",
+        path: "/market",
+        expiry: DateTime.UtcNow.AddDays(7));
+
+        driver.Manage().Cookies.AddCookie(cookie);
+
+        driver.Navigate().Refresh();
+
         var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
         wait.Until(ExpectedConditions.ElementExists(By.XPath($"//a[starts-with(@id, '{Constants.RESULTLINK}')]")));
 
